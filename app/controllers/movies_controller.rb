2ffc53ba06ea8881e1984d646d50fddd68a77b67
庞羽 <py008@pyMacBook-Pro.local>
2017-04-02 14:32:32 +0800

    implement movies#new

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,17 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
-  end  
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description)
+  end    
+
 end

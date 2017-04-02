c3afe87864c189f4a7ef700bcb9fda4191a764b5
庞羽 <py008@pyMacBook-Pro.local>
2017-04-02 14:42:08 +0800

    implement movies#create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,11 +7,20 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end  
+
 
   private
 
   def movie_params
     params.require(:movie).permit(:title, :description)
-  end    
+  end
 
 end

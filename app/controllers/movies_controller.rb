3da99fbdb9283f9360c35cb48e8c0c65e173b0fe
庞羽 <py008@pyMacBook-Pro.local>
2017-04-03 14:54:56 +0800

    when created a movie, join it automatically

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
-  before_action :find_movie_and_check_permission, only: [:edit, :update, :destroy]
+  before_action :find_movie_and_check_permission, only: [:edit, :update, :destroy,:join, :quit]
   def index
     @movies = Movie.all
   end
@@ -19,6 +19,7 @@ class MoviesController < ApplicationController
     @movie.user = current_user
 
     if @movie.save
+      current_user.join!(@moive)
       redirect_to movies_path
     else
       render :new
@@ -40,6 +41,33 @@ class MoviesController < ApplicationController
     @movie.destroy
     redirect_to movies_path, alert: "Movie deleted"
   end
+
+  def join
+    @movie = Movie.find(params[:id])
+
+    if !current_user.is_member_of?(@movie)
+      current_user.join!(@movie)
+      flash[:notice] = "收藏本影片成功！"
+    else
+      flash[:warning] = "你已经收藏过本影片了！"
+    end
+
+    redirect_to movie_path(@movie)
+  end
+
+  def quit
+    @movie = Movie.find(params[:id])
+
+    if current_user.is_member_of?(@movie)
+      current_user.quit!(@movie)
+      flash[:alert] = "已取消收藏本影片！"
+    else
+      flash[:warning] = "你都没有收藏过本影片，怎么能取消呢"
+    end
+
+    redirect_to movie_path(@movie)
+  end
+
 
   private
 

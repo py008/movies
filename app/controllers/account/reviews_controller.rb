8057e0cd5670192a46206322fbd792307a801a63
庞羽 <py008@pyMacBook-Pro.local>
2017-04-03 17:01:14 +0800

    implement my reviews

--- app/controllers/account/reviews_controller.rb
+++ app/controllers/account/reviews_controller.rb
@@ -2,6 +2,35 @@ class Account::ReviewsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @reviews = current_user.reviews
+    @reviews = current_user.reviews.recent.paginate(:page => params[:page], :per_page => 5)
+  end
+
+  def edit
+    @movie = Moive.find(params[:movie_id])
+    @review = Rview.find(params[:id])
+  end
+
+  def updated
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to reviews_path(@review), notice: "Updated review"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path(@review), alert: "Review deleted"
+  end
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create]
-  
+
   def new
     @movie = Movie.find(params[:movie_id])
     @review = Review.new
@@ -16,6 +16,28 @@ class ReviewsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to account_reviews_path(@review), notice: "Updated review"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to account_reviews_path(@review), alert: "Review deleted"
   end
 
   private
